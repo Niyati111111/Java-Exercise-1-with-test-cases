7aa34cff06fa6934c281060fd4223a238dfa150b
Niyati111111 <51950233+Niyati111111@users.noreply.github.com>
2019-07-01 16:20:14 +0530

    Update CharacterTypeTest.java

--- src/main/test/com/stackroute/javaexercise1/CharacterTypeTest.java
+++ src/main/test/com/stackroute/javaexercise1/CharacterTypeTest.java
@@ -2,37 +2,21 @@ package com.stackroute.javaexercise1;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
-import org.junit.BeforeClass;
-import org.junit.AfterClass;
 import static org.junit.Assert.*;
 
 public class CharacterTypeTest {
 
     CharacterType characterType;
 
-
-    @BeforeClass
-    public static void setUpBeforeClass() {
-        System.out.println("Before class");
-
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() {
-        System.out.println("After class");
-
-    }
-
-
     @Before
     public void setUp() {
-        System.out.println("Before");
+      
         characterType = new CharacterType();
     }
 
     @After
     public void tearDown() {
-        System.out.println("After");
+       
         characterType = null;
     }
 
