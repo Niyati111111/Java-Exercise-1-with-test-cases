7731fa217c984dddcef94bdcd1b32fc244107a0d
Niyati111111 <51950233+Niyati111111@users.noreply.github.com>
2019-07-01 16:29:14 +0530

    Update RepeatNTimesTest.java

--- src/main/test/com/stackroute/javaexercise1/RepeatNTimesTest.java
+++ src/main/test/com/stackroute/javaexercise1/RepeatNTimesTest.java
@@ -2,37 +2,22 @@ package com.stackroute.javaexercise1;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
-import org.junit.BeforeClass;
-import org.junit.AfterClass;
 import static org.junit.Assert.*;
 
 public class RepeatNTimesTest {
 
     RepeatNTimes repeatNTimes;
 
-    @BeforeClass
-    public static void setUpBeforeClass() {
-        System.out.println("Before class");
-
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() {
-        System.out.println("After class");
-
-    }
-
-
     @Before
     public void setUp() {
-        System.out.println("Before");
+       
         repeatNTimes = new RepeatNTimes();
     }
 
     @After
     public void tearDown() {
-        System.out.println("After");
         repeatNTimes = null;
+       
     }
 
     @Test
