572cd0e5a09797d39e9fef1980f7a1709df34ddf
Niyati111111 <51950233+Niyati111111@users.noreply.github.com>
2019-07-01 16:25:29 +0530

    Update GuessGameTest.java

--- src/main/test/com/stackroute/javaexercise1/GuessGameTest.java
+++ src/main/test/com/stackroute/javaexercise1/GuessGameTest.java
@@ -2,41 +2,26 @@ package com.stackroute.javaexercise1;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
-import org.junit.BeforeClass;
-import org.junit.AfterClass;
 import static org.junit.Assert.*;
 
 public class GuessGameTest {
 
     GuessGame guessGame ;
 
-    @BeforeClass
-    public static void setUpBeforeClass() {
-        System.out.println("Before class");
-
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() {
-        System.out.println("After class");
-
-    }
-
-
     @Before
     public void setUp() {
-        System.out.println("Before");
+       
         guessGame = new GuessGame();
     }
 
     @After
     public void tearDown() {
-        System.out.println("After");
+     
         guessGame = null;
     }
 
     @Test
-    public void givenTargetShouldGenerateRandomUntilTargetMatchesRandom() {
+    public void givenTargetShouldReturnWhenMatches() {
         //arrange
 
         //act
@@ -56,7 +41,7 @@ public class GuessGameTest {
     }
 
     @Test
-    public void givenNumberGreaterThan100ShouldReturnErrorMessage() {
+    public void givenOutOfRangeShouldReturnErrorMessage() {
         //arrange
 
         //act
