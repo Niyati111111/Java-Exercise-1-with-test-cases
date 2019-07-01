ad93125f8bd2d0972fcda36132e2dfe9ae1f1868
Niyati111111 <51950233+Niyati111111@users.noreply.github.com>
2019-07-01 16:23:34 +0530

    Update EvenOddTest.java

--- src/main/test/com/stackroute/javaexercise1/EvenOddTest.java
+++ src/main/test/com/stackroute/javaexercise1/EvenOddTest.java
@@ -2,37 +2,21 @@ package com.stackroute.javaexercise1;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
-import org.junit.BeforeClass;
-import org.junit.AfterClass;
 import static org.junit.Assert.*;
 
 public class EvenOddTest {
 
-     EvenOdd evenOdd;
-
-
-    @BeforeClass
-    public static void setUpBeforeClass() {
-        System.out.println("Before class");
-
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() {
-        System.out.println("After class");
-
-    }
-
+    EvenOdd evenOdd;
 
     @Before
     public void setUp() {
-        System.out.println("Before");
+        
         evenOdd = new EvenOdd();
     }
 
     @After
     public void tearDown() {
-        System.out.println("After");
+        
         evenOdd = null;
     }
 
@@ -57,7 +41,7 @@ public class EvenOddTest {
     }
 
     @Test
-    public void givenOutOfRangeShouldPrintNeitherTomNorJerry() {
+    public void givenOutOfRangeShouldPrintNeither() {
         //arrange
 
         //act
